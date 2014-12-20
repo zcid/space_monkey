aeb895838847e4ffa504513a6b350ba9f500ac1b
JPark <park.joonw@gmail.com>
2014-12-20 14:56:28 -0700

    user_id for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -19,6 +19,7 @@ class ReviewsController < ApplicationController
 
   def create
   	@review = @product.reviews.build(review_params)
+    @review.user_id = current_user.id
   	if @review.save
   		redirect_to @product
   	else
