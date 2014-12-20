26f6524d9f021bb7698111805b56a50e52fab1cb
Topher Blair <bisonproperties@mac.com>
2014-12-20 14:09:53 -0700

    added user authentication to reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,7 @@
 class ReviewsController < ApplicationController
   before_action :set_product, only: [:new, :update, :create, :destroy, :edit]
   before_action :set_review, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:edit, :update, :destroy, :new]
 
   def index
   	@reviews = Review.all
